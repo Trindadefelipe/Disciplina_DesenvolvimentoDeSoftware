11c4bf5f5d9cd0fa113ddbf98812d7e0bbcee7d1
Felipe Trindade <felipe_biel3@hotmail.com>
2026-04-19 18:10:29 -0300

    Finalização de projeto Ecommerce

--- projeto_Ecommerce/ecommerce.java
+++ projeto_Ecommerce/ecommerce.java
@@ -1,68 +1,192 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Produto {
-    String nomeProduto;
-    Double valorProduto;
+    private String nomeProduto; // Encapsulando, necessitando utilizar get e set
+    private double valorProduto;
+    double percentual = 0.10;
+    private double valorLiquido = 0.0;
 
-    public Produto(String nomeProduto, Double valorProduto) {
+    public Produto(String nomeProduto, double valorProduto) {
         this.nomeProduto = nomeProduto;
         this.valorProduto = valorProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public double getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public void setValorProduto(double valorProduto) {
+        if (valorProduto >= 0) {
+            this.valorProduto = valorProduto;
+        } else {
+            System.out.println("Valor de produto inválido!!");
+        }
+    }
+
+    void aplicarDesconto(double percentual) {
+        valorLiquido = valorProduto - (valorProduto * percentual / 100);
+        this.valorProduto = valorLiquido;
     }
 }
 
 class ProdutoFisico extends Produto {
-    Double peso;
+    private double peso;
 
-    public ProdutoFisico(String nomeProduto, Double valorProduto, Double peso) {
+    public ProdutoFisico(String nomeProduto, double valorProduto, double peso) {
         super(nomeProduto, valorProduto);
         this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        if (peso >= 0) {
+            this.peso = peso;
+        } else {
+            System.out.println("Peso de produto inválido!");
+        }
     }
 }
 
 class ProdutoDigital extends Produto {
-    Double tamanhoArquivo;
+    private double tamanhoArquivo;
 
-    public ProdutoDigital(String nomeProduto, Double valorProduto, Double tamanhoArquivo) {
+    public ProdutoDigital(String nomeProduto, double valorProduto, double tamanhoArquivo) {
         super(nomeProduto, valorProduto);
         this.tamanhoArquivo = tamanhoArquivo;
+    }
+
+    public double getTamanhoArquivo() {
+        return tamanhoArquivo;
+    }
+
+    public void setTamanhoArquivo(double tamanhoArquivo) {
+        if (tamanhoArquivo >= 0) {
+            this.tamanhoArquivo = tamanhoArquivo;
+        } else {
+            System.out.println("Tamanho do arquivo inválido!");
+        }
     }
 }
 
 class Cliente {
-    String nome;
+    private String nomeCliente;
+
+    // construtor
+    public Cliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
 
 }
 
 class Carrinho {
-    ArrayList<Produto> carrinho = new ArrayList<>();
-    Double valorTotal = 0.0;
+    private ArrayList<Produto> produtos;
 
+    // Construtor
+    public Carrinho() {
+        this.produtos = new ArrayList<>();
+    }
+
+    // criando get para retornar lista de produtos
+    public ArrayList<Produto> getProdutosCarrinho() {
+        return this.produtos;
+    }
+
+    // Método para adicionar um produto no carrinho
     void adicionarProduto(Produto p) {
-        carrinho.add(p);
+        if (p.getValorProduto() > 0) {
+            this.produtos.add(p);
+            System.out.println(p.getNomeProduto() + " adicionado no carrinho!");
+        } else {
+            System.out.println("Valor de produto inválido! Não foi adicionado ao carrinho!");
+        }
     }
 
+    // Método para remover um produto do carrinho
+    void removerProduto(Produto p) {
+        if (!this.produtos.remove(p)) {
+            System.out.println("Produto não encontrado no carrinho!");
+        }
+    }
+
+    // Método mostrar/listar produtos
     void listarProdutos() {
-        for (Produto produto : carrinho) {
+
+        if (produtos.isEmpty()) {
+            System.out.println("Nenhum produto no carrinho!");
+        } else {
             System.out.println("======Listagem de Produtos do Carrinho====");
-            System.out.println("Produto: " + produto.nomeProduto);
-            System.out.println("Valor: R$" + produto.valorProduto);
+            for (Produto p : produtos) {
+                System.out.println("-------------------------");
+                System.out.println("Produto: " + p.getNomeProduto());
+                System.out.println("Valor: R$" + p.getValorProduto());
 
-            if (produto instanceof ProdutoDigital pd) {
-                System.out.println("Tamanho do arquivo: " + pd.tamanhoArquivo + "MB");
-            }
+                // utilização de instanceof para confirmar o tipo do objeto
+                if (p instanceof ProdutoDigital pd) {
+                    System.out.println("Tamanho do arquivo: " + pd.getTamanhoArquivo() + "MB");
+                }
 
-            if (produto instanceof ProdutoFisico pf) {
-                System.out.println("Peso do produto: " + pf.peso + "Kg");
+                if (p instanceof ProdutoFisico pf) {
+                    System.out.println("Peso do produto: " + pf.getPeso() + "Kg");
+                }
             }
         }
+
     }
 
-    void calcularTotal() {
-        for (Produto produto : carrinho) {
-            valorTotal += produto.valorProduto;
+    // Método calcular total de produto do carrinho
+    double calcularTotal() {
+        double valorTotal = 0.0;
+        for (Produto p : produtos) {
+            valorTotal += p.getValorProduto();
         }
-        System.out.println("=====Total do Carrinho=====");
+        System.out.println("===Total do Carrinho===");
         System.out.println("Valor total do carrinho: R$" + valorTotal);
+        return valorTotal;
+    }
+}
+
+class Pedido {
+    private Cliente cliente;
+    private Carrinho carrinho;
+
+    // construtor
+    public Pedido(Cliente cliente, Carrinho carrinho) {
+        this.cliente = cliente;
+        this.carrinho = carrinho;
+    }
+
+    public Cliente getClientePedido() {
+        return this.cliente;
+    }
+
+    public Carrinho getCarrinhoCliente() {
+        return this.carrinho;
+    }
+
+    // Método mostrar resumo
+    void mostrarResumo() {
+        System.out.println();
+        System.out.println("===Encerrando===");
+        System.out.println("Obrigado por comprar! Seu resumo do pedido:");
+        System.out.println("Cliente: " + this.cliente.getNomeCliente());
+        this.carrinho.listarProdutos();
+        this.carrinho.calcularTotal();
     }
 }
 
@@ -70,53 +194,71 @@ public class ecommerce {
     public static void main(String[] args) {
         System.out.println();
 
-        Produto produto1 = new Produto();
-        Produto produto2 = new Produto();
-        Produto produto3 = new Produto();
+        Scanner entrada = new Scanner(System.in);
 
-        ProdutoFisico produtofisico1 = new ProdutoFisico();
-        ProdutoDigital produtodigital1 = new ProdutoDigital();
+        // Criação de produtos
+        Produto produto1 = new Produto("Camisa Linho", 129.99);
+        Produto produto2 = new Produto("Calça Alfaiataria", 154.50);
 
-        produtofisico1.nomeProduto = "Televisão";
-        produtofisico1.valorProduto = 1500.00;
-        produtofisico1.peso = 25.0;
+        // Criação de produtos Físicos
+        ProdutoFisico produtofisico1 = new ProdutoFisico("Televisão 50 polegadas", 1500, 12);
+        ProdutoFisico produtofisico2 = new ProdutoFisico("Monitor 24 polegadas 144hz Gamer", 630.99, 4);
 
-        produtodigital1.nomeProduto = "GTA 6";
-        produtodigital1.valorProduto = 500.00;
-        produtodigital1.tamanhoArquivo = 1000000.00;
+        // Criação de produtos Digitais
+        ProdutoDigital produtodigital1 = new ProdutoDigital("GTA 6", 899.99, 1000000);
+        ProdutoDigital produtodigital2 = new ProdutoDigital("Call Of Dutty Warzone", 150.50, 100);
 
-        produto1.nomeProduto = "Banana";
-        produto1.valorProduto = 9.50;
-        produto2.nomeProduto = "Alface";
-        produto2.valorProduto = 4.50;
-        produto3.nomeProduto = "Abacaxi";
-        produto3.valorProduto = 7.95;
+        // Aplicando desconto em um produto
+        produtodigital2.aplicarDesconto(100);
+        produto2.aplicarDesconto(10);
 
+        System.out.print("Digite seu nome: ");
+        String nomeDigitado = entrada.nextLine();
+        Cliente cliente1 = new Cliente(nomeDigitado);
+        // Criando carrinho do cliente
         Carrinho carrinho1 = new Carrinho();
-        Carrinho carrinho2 = new Carrinho();
-        Carrinho carrinho3 = new Carrinho();
 
-        // adicionando produtos carrinho 1
-        carrinho1.adicionarProduto(produto1);
-        carrinho1.adicionarProduto(produto2);
-        carrinho1.adicionarProduto(produto3);
-        carrinho1.adicionarProduto(produto3);
+        // Menu de exibição
+        int opcao;
+        do {
+            System.out.println("Bem vindo, " + cliente1.getNomeCliente() + "!");
+            System.out.println("==== Ecommerce - Felipe Trindade ====");
+            System.out.println(
+                    "Escolha o produtos desejado:\n1 - Camisa Linho - R$129,90 \n2 - Calça Alfaiataria - R$154,50 \n3 - Televisão 50' - R$1.500,00 \n4 - Monitor 24' 144hz Gamer - R$630,99 \n5 - GTA 6 - R$899,99 \n6 - Call Of Dutty Warzone - R$150,50 \n7 - Finalizar compra \n0 - Sair");
+            opcao = entrada.nextInt();
 
-        // adicionando produtos carrinho 2
-        carrinho2.adicionarProduto(produto2);
-        carrinho2.adicionarProduto(produto3);
+            switch (opcao) {
+                case 1:
+                    carrinho1.adicionarProduto(produto1);
+                    break;
+                case 2:
+                    carrinho1.adicionarProduto(produto2);
+                    break;
+                case 3:
+                    carrinho1.adicionarProduto(produtofisico1);
+                    break;
+                case 4:
+                    carrinho1.adicionarProduto(produtofisico2);
+                    break;
+                case 5:
+                    carrinho1.adicionarProduto(produtodigital1);
+                    break;
+                case 6:
+                    carrinho1.adicionarProduto(produtodigital2);
+                    break;
+                case 7:
+                    Pedido pedido1 = new Pedido(cliente1, carrinho1);
+                    pedido1.mostrarResumo();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        } while (opcao != 0 && opcao != 7);
 
-        // adicionando produtos carrinho 3
-        carrinho3.adicionarProduto(produtofisico1);
-        carrinho3.adicionarProduto(produtodigital1);
-
-        carrinho1.listarProdutos();
-        carrinho2.listarProdutos();
-        carrinho3.listarProdutos();
-
-        carrinho1.calcularTotal();
-        carrinho2.calcularTotal();
-        carrinho3.calcularTotal();
+        entrada.close();
     }
-
 }
